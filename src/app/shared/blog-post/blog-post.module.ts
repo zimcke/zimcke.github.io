3a69750c1969c940cf43703897a9843ec03e6668
AlexanderTang <alexandertang357@hotmail.com>
2019-11-11 01:25:53 +0100

    implement comment mailto

--- src/app/shared/blog-post/blog-post.module.ts
+++ src/app/shared/blog-post/blog-post.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import {BlogPostComponent} from './blog-post.component';
 import {BlogSidebarModule} from '../blog-sidebar/blog-sidebar.module';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, BlogSidebarModule, RouterModule],
+  imports: [CommonModule, BlogSidebarModule, RouterModule, FormsModule],
   exports: [BlogPostComponent],
   declarations: [BlogPostComponent]
 })
