89918b35dbc515a94e7ccd512ba426baf5704c50
AlexanderTang <alexandertang357@hotmail.com>
2019-11-11 02:17:16 +0100

    set default blog page 1

--- src/app/shared/blog-overview/blog-overview.component.ts
+++ src/app/shared/blog-overview/blog-overview.component.ts
@@ -1,19 +1,36 @@
 'use strict';
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IBlogPost} from '../interfaces';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'cm-blog-overview',
     templateUrl: './blog-overview.component.html'
 })
-export class BlogOverviewComponent {
+export class BlogOverviewComponent implements OnInit {
 
     @Input() blogTitle: string;
-    @Input() blogPosts: IBlogPost[];
+    @Input() blogPosts: IBlogPost[] = [];
 
-    constructor() {}
+    currentPage: number;
+    pageSize: number = 5;
+
+    constructor(private activatedRoute: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.setCurrentPage();
+    }
 
     postImagePresent(blogPost: IBlogPost): boolean {
         return blogPost.postImageName && blogPost.postImageName !== '';
+    }
+
+    private setCurrentPage(): void {
+        this.currentPage = +this.activatedRoute.snapshot.queryParamMap.get('page');
+        if (isNaN(this.currentPage) || this.currentPage < 1 ||
+            (this.currentPage - 1) * this.pageSize + 1 > this.blogPosts.length) {
+            this.currentPage = 1;
+        }
     }
 }
