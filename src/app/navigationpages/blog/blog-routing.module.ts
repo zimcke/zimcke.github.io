530661e59a92dc0769e7bd3fcb26b1db6799f445
AlexanderTang <alexandertang357@hotmail.com>
2019-11-11 16:37:56 +0100

    update README

--- src/app/navigationpages/blog/blog-routing.module.ts
+++ src/app/navigationpages/blog/blog-routing.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: 'dev', data: {breadcrumb: 'Dev Blog'}, loadChildren: () => import('./dev/developer-blog.module').then(m => m.DeveloperBlogModule)},
-    {path: 'game', data: {breadcrumb: 'Game Blog'}, loadChildren: () => import('./gaming/gaming-blog.module').then(m => m.GamingBlogModule)},
+    {path: 'dev', data: {breadcrumb: 'Developer Blog'}, loadChildren: () => import('./dev/developer-blog.module').then(m => m.DeveloperBlogModule)},
+    {path: 'game', data: {breadcrumb: 'Gaming Blog'}, loadChildren: () => import('./gaming/gaming-blog.module').then(m => m.GamingBlogModule)},
     {path: '**', redirectTo: 'dev'} // catch any unfound routes and redirect to dev blog page
 ];
 
